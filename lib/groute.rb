5df76007f33feb1a277680d5309bcc8f099f36f4
Pocket7878 <poketo7878@gmail.com>
2020-08-09 20:50:29 +0900

    refactor: Remove unnecessally Route model.

--- lib/groute.rb
+++ lib/groute.rb
@@ -6,7 +6,6 @@ require 'groute/distance'
 require 'groute/strait_distance_calculator'
 require 'groute/google_directions_distance_calculator'
 require 'groute/google_directions_distance_calculator/api_status_not_ok_error'
-require 'groute/route'
 require 'groute/config'
 
 module Groute
