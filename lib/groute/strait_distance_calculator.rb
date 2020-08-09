f1fe377cbccf8d7dc2c094295c28df4bbdd87b91
Pocket7878 <poketo7878@gmail.com>
2020-08-09 18:53:11 +0900

    feature: ヒュベニの公式で直線距離を計算

--- lib/groute/strait_distance_calculator.rb
+++ lib/groute/strait_distance_calculator.rb
@@ -1,5 +1,14 @@
+# frozen_string_literal: true
+
 module Groute
+  # 2地点間の地球上での直線距離を計算する
   class StraitDistanceCalculator
+    # 地球の赤道半径
+    EARTH_EQUATORIAL_RADIUS_METER = 6_378_137.0
+    # 地球の極半径
+    EARTH_POLAR_RADIUS_METER = 6_356_752.314140
+    private_constant :EARTH_EQUATORIAL_RADIUS_METER, :EARTH_POLAR_RADIUS_METER
+
     class << self
       # @param [Groute::LatLng] origin
       # @param [Groute::LatLng] destination
@@ -16,13 +25,52 @@ module Groute
       @destination = destination
     end
 
+    # ヒュベニの公式にもとづいて距離をもとめる
     # @return [Groute::Distance]
     def strait_distance
-      Distance.new(0)
+      meter = Math.sqrt(
+        (latitude_radian_difference * meridian_radius_curvature)**2 +
+          (longitude_radian_difference * prime_vertical_radius * Math.cos(latitude_radian_average))**2
+      )
+      Groute::Distance.new(meter)
     end
 
     private
 
     attr_reader :origin, :destination
+
+    def latitude_radian_difference
+      origin.latitude_radian - destination.latitude_radian
+    end
+
+    def longitude_radian_difference
+      origin.longitude_radian - destination.longitude_radian
+    end
+
+    def latitude_radian_average
+      (origin.latitude_radian + destination.latitude_radian) / 2.0
+    end
+
+    def earth_eccentricity
+      Math.sqrt(
+        (EARTH_EQUATORIAL_RADIUS_METER**2 - EARTH_POLAR_RADIUS_METER**2) / (EARTH_EQUATORIAL_RADIUS_METER**2).to_f
+      )
+    end
+
+    # 卯酉線曲線半径
+    def prime_vertical_radius
+      EARTH_EQUATORIAL_RADIUS_METER / w
+    end
+
+    def w
+      Math.sqrt(
+        1.0 - earth_eccentricity**2 * Math.sin(latitude_radian_average)**2
+      )
+    end
+
+    # 子午線曲率半径
+    def meridian_radius_curvature
+      (EARTH_EQUATORIAL_RADIUS_METER * (1.0 - earth_eccentricity**2)) / (w**3)
+    end
   end
 end
--- spec/groute/strait_distance_calculator_spec.rb
+++ spec/groute/strait_distance_calculator_spec.rb
@@ -1,15 +1,25 @@
+# frozen_string_literal: true
+
 RSpec.describe Groute::StraitDistanceCalculator do
   describe 'call' do
-    it "LatLngを二つ取る" do
+    it 'LatLngを二つ取る' do
       origin = Groute::LatLng.new(35.0, 135.0)
       destination = Groute::LatLng.new(30.0, 135.0)
       expect(Groute::StraitDistanceCalculator.call(origin, destination)).not_to be nil
     end
 
-    it "Groute::Distanceを返す" do
+    it 'Groute::Distanceのインスタンスを返す' do
       origin = Groute::LatLng.new(35.0, 135.0)
       destination = Groute::LatLng.new(30.0, 135.0)
       expect(Groute::StraitDistanceCalculator.call(origin, destination)).to be_an_instance_of(Groute::Distance)
+    end
+
+    it 'Groute::Distanceの距離をメートル単位で返す' do
+      shibuya = Groute::LatLng.new(35.658034, 139.701636)
+      roppoingi = Groute::LatLng.new(35.662725, 139.731216)
+      expect(Groute::StraitDistanceCalculator.call(shibuya, roppoingi)).to satisfy do |d|
+        d.distance >= 2000 && d.distance <= 3000
+      end
     end
   end
 end
