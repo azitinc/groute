f998ab7d6a8e0f5ff77c8dcc9278461bed1b25d1
Pocket7878 <poketo7878@gmail.com>
2020-08-09 17:41:59 +0900

    feature: Define Groute::Route

---
+++ lib/groute/route.rb
@@ -0,0 +1,22 @@
+module Groute
+  # 移動時間と、移動距離で表現される経路
+  class Route
+    # @return [Float] 移動時間(分)
+    attr_reader :minutes
+    # @return [Groute::Distance] 距離
+    attr_reader :distance
+
+    # @param [Float] minutes 移動時間 (分)
+    # @param [Groute::Distance] distance 距離
+    def initialize(minutes, distance)
+      @minutes = minutes
+      @distance = distance
+    end
+
+    def ==(other)
+      self.class == other.class && minutes == other.minutes && distance == other.distance
+    end
+
+    alias eql? ==
+  end
+end
