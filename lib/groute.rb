9ea4e02bc698f2acef671b54322925e5af0bd617
Pocket7878 <poketo7878@gmail.com>
2020-08-09 19:25:46 +0900

    feature: Add Documentation to Groute::Config

--- lib/groute.rb
+++ lib/groute.rb
@@ -8,10 +8,15 @@ require 'groute/route'
 require 'groute/config'
 
 module Groute
+  # @return [Groute::Config] 現在の設定値
   def self.config
     @config ||= Groute::Config.new
   end
 
+  # @example ブロックをつかって設定値を更新する
+  #   Groute.configure do |conf|
+  #     conf.google_map_api_key = "<YOUR GOOGLE MAP API KEY HERE>"
+  #   end
   def self.configure(&_block)
     yield(config) if block_given?
   end
--- lib/groute/config.rb
+++ lib/groute/config.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 module Groute
+  # 設定を管理する
   class Config
+    # @return [String] Google Map API Key
     attr_accessor :google_map_api_key
 
     def initialize
